65647421118eee119d3079aeea3561a4d596aba3
cnxsu <121377609+cnxsu@users.noreply.github.com>
2024-05-11 15:33:49 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const Application = require('@waline/vercel');
-
-module.exports = Application({
-  plugins: [],
-  async postSave(comment) {
-    // do what ever you want after comment saved
-  },
-});
